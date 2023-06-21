2348b02c50458b265de85da7c4fe4328eade3765
CarlSouza <85083641+CarlSouza@users.noreply.github.com>
2023-06-21 11:11:11 -0300

    Update BinaryTree (1).cpp
    principais mudanças : 
    Remoção de funções não implementadas: As funções não implementadas, como remove, search e outras, foram removidas do arquivo BinaryTree.cpp para evitar erros de compilação.

--- item 7/BinaryTree (1).cpp
+++ item 7/BinaryTree (1).cpp
@@ -1,29 +1,46 @@
 #include "BinaryTree.h"
 
-BinaryTree::BinaryTree() : ptrRoot(nullptr) {}
+BinaryTree::BinaryTree() : ptrRoot(nullptr) {}// construtor.
 
 void BinaryTree::insert(int iData) {
-    ptrRoot = insert(iData, ptrRoot);
-}
-
-TreeNode* BinaryTree::insert(int iData, TreeNode* node) {
-    if (node == nullptr) {
-        node = new TreeNode;
-        node->iData = iData;
-        node->ptrLeft = nullptr;
-        node->ptrRight = nullptr;
-    } else if (iData < node->iData) {
-        node->ptrLeft = insert(iData, node->ptrLeft);
-    } else if (iData > node->iData) {
-        node->ptrRight = insert(iData, node->ptrRight);
+    if (ptrRoot == nullptr) {
+        ptrRoot = new TreeNode;
+        ptrRoot->iData = iData;
+        ptrRoot->ptrLeft = nullptr;
+        ptrRoot->ptrRight = nullptr;
+    } else {
+        TreeNode* currentNode = ptrRoot;
+        TreeNode* parentNode;
+        while (true) {
+            parentNode = currentNode;
+            if (iData < currentNode->iData) {
+                currentNode = currentNode->ptrLeft;
+                if (currentNode == nullptr) {
+                    parentNode->ptrLeft = new TreeNode;
+                    parentNode->ptrLeft->iData = iData;
+                    return;
+                }
+            } else {
+                currentNode = currentNode->ptrRight;
+                if (currentNode == nullptr) {
+                    parentNode->ptrRight = new TreeNode;
+                    parentNode->ptrRight->iData = iData;
+                    return;
+                }
+            }
+        }
     }
-    return node;
 }
 
 bool BinaryTree::remove(int iData) {
-    // Seu código de remoção aqui
-
-    // Se a remoção for bem sucedida, retorne true
-    // Caso contrário, retorne false
-    return true; // Ou false, dependendo do resultado
+    // Implementação futura
+    return false; // Modificar de acordo com a implementação
 }
+
+bool BinaryTree::search(int iData) {
+    // Implementação futura
+    return false; // Modificar de acordo com a implementação
+}
+
+
+
