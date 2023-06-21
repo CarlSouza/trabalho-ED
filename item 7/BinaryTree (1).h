d2631103498ac7e449eda8ddae525eecdb56bc83
CarlSouza <85083641+CarlSouza@users.noreply.github.com>
2023-06-21 11:15:22 -0300

    Update BinaryTree (1).h
    principais mudanças:
    Adição de novos métodos: Foram adicionados os protótipos dos  métodos como : "insert" O " remove" e" search" mas eles ainda não possuem implementação.

--- item 7/BinaryTree (1).h
+++ item 7/BinaryTree (1).h
@@ -4,12 +4,14 @@
 #include "TreeNode.h"
 
 struct BinaryTree {
-    TreeNode* ptrRoot;
+    TreeNode* ptrRoot; // Ponteiro para a raiz da árvore
 
-    BinaryTree();
-    void insert(int iData);
-    TreeNode* insert(int iData, TreeNode* node);
-    bool remove(int iData);
+    BinaryTree(); // Construtor da árvore
+
+    void insert(int iData); // Função para inserir um elemento na árvore
+    bool remove(int iData); // Função para remover um elemento da árvore. Retorna true se a remoção for bem-sucedida.
+    bool search(int iData); // Função para pesquisar um elemento na árvore. Retorna true se o elemento estiver presente.
 };
 
 #endif  // BINARYTREE_H
+
