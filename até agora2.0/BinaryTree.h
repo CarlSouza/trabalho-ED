ef9e77da6d5c56a8c0d913a91f84ec9b79756255
CarlSouza <85083641+CarlSouza@users.noreply.github.com>
2023-06-23 11:16:46 -0300

    Update BinaryTree.h
    adicionei os itens :                                                                     
    10.Uma das opções deve informar se a árvore é completa
    
    11.Uma das opções deve informar se a árvore é perfeita
    ainda preciso verificar se todas as variáveis estão na notação húngara

--- até agora2.0/BinaryTree.h
+++ até agora2.0/BinaryTree.h
@@ -23,6 +23,24 @@ struct BinaryTree {
     bool isComplete(); // Verifica se a árvore é completa
     bool isPerfect(); // Verifica se a árvore é perfeita
     std::vector<int> convertToVector(); // Converte a árvore em um vetor
+    
+    // Funções para obter tamanho e altura da árvore
+    int height();  // função para obter a altura da árvore
+    int size();  // função para obter o tamanho da árvore
+
+    // Novos métodos para auxiliar na remoção
+    TreeNode* findMin(TreeNode* node);  // Este método retorna o nó com o menor valor na árvore
+    TreeNode* remove(int iData, TreeNode* node);  // Este é o nosso método de remoção recursivo. Ele percorre a árvore e remove o nó com o valor fornecido
+
+    // Funções auxiliares que operam diretamente em nós
+    int height(TreeNode* node);  // função para obter a altura de um nó específico
+    int size(TreeNode* node);  // função para obter o tamanho de um nó específico
+    
+    // Funções auxiliares para verificar se a árove é completa e perfeita de um nó especifico
+    bool isComplete(TreeNode* node, int iIndex, int iNumNodes);  // função para verificar se a árvore a partir do nó é completa
+    bool isPerfect(TreeNode* node, int iDepth, int iLevel = 0);  // função para verificar se a árvore a partir do nó é perfeita
+    
 };
+
 
 #endif  // BINARYTREE_H
