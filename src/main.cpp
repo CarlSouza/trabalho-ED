9787864d1d6b8961748b86631807b23bd51dec5f
Rian Freitas <rianfreitassilva@gmail.com>
2023-06-30 17:53:26 -0300

    removing unnecessary #includes

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,6 @@
 #include "../headers/BinaryTree.hpp"
 #include "../headers/Utils.hpp"
 
-// // linhas a serem ignoradas no commit
-// #include "BinaryTree.cpp"
-// #include "Utils.cpp"
-// #include "BubbleSort.cpp"
-// #include "InsertionSort.cpp"
-// #include "SelectionSort.cpp"
-// #include "ShellSort.cpp"
-// #include "LinkedList.cpp"
-// #include "TreeNode.cpp"
-
 using std::cout;
 using std::cin;
 using std::endl;
