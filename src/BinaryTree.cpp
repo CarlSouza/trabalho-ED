28333a6411e81e74f42cfb570bb69aa5ae0a060b
ari <AriOliv>
2023-06-30 21:30:30 -0300

    Modularizando queue

--- src/BinaryTree.cpp
+++ src/BinaryTree.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
 #include <iostream>
-#include <queue>
 
 #include "../headers/BinaryTree.hpp"
+#include "../headers/Queue.hpp"
 
 using std::string;
 using std::endl;
@@ -232,63 +232,6 @@ void BinaryTree::buildFromUserInput(const std::vector<int> &userInput) {
     // Itera sobre cada valor fornecido pelo usuário e insere na árvore.
     for (const int &value : userInput) {
         insert(value);
-    }
-}
-
-struct QueueNode {
-    int iData;
-    struct QueueNode* ptrNext;
-};
-
-struct Queue {
-    struct QueueNode* ptrFront;
-    struct QueueNode* ptrRear;
-};
-
-struct QueueNode* newQueueNode(int iData) {
-    struct QueueNode* ptrTemp = (struct QueueNode*) malloc(sizeof(struct Queue));
-    ptrTemp->iData = iData;
-    ptrTemp->ptrNext = nullptr;
-    
-    return ptrTemp;
-}
-
-struct Queue* newQueue() {
-    // Criar um struct convertendo um ponteiro de void para o struct Node
-    struct Queue* ptrTemp = (struct Queue*) malloc(sizeof(struct Queue));
-    ptrTemp->ptrFront = nullptr;
-    ptrTemp->ptrRear = nullptr;
-    
-    return ptrTemp;
-};
-
-// função de insert com um nome diferente :/
-void enQueue(struct Queue* queue, int iValue) {
-    struct QueueNode* ptrTemp = newQueueNode(iValue);
-    if (queue->ptrRear == nullptr) {
-        queue->ptrFront = ptrTemp;
-        queue->ptrRear = ptrTemp;
-    } else {
-        // lembra que o último da fila tá apontando pra nullptr? nós fazemos o último apontar pra esse novo elemento
-        queue->ptrFront->ptrNext = ptrTemp;
-        // Agora temos que fazer o struct da fila reconhecer esse novo elemento como último
-        queue->ptrRear = ptrTemp;
-    }
-}
-
-void deQueue(struct Queue* queue) {
-    // tanto faz perguntae se o último ou o primeiro estão vazios
-    if (queue->ptrFront == nullptr) {
-        cout << "Fila vazia." << endl;
-    } else {
-        struct QueueNode* ptrTemp = queue->ptrFront;
-        
-        queue->ptrFront = queue->ptrFront->ptrNext;
-        // para o caso de se remover o último elemento, precisamos modificar o ponto rear antes de apagar
-        if (queue->ptrFront == nullptr) queue->ptrRear = nullptr;
-        cout << "deQueue: " << ptrTemp->iData << endl;
-        
-        free(ptrTemp);
     }
 }
 
